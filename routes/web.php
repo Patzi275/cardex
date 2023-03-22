80cb9440e98bcd85147eddafccb3c779fadbd25a
patrick <patrickzocli2021@gmail.com>
2023-03-22 20:27:56 +0100

    Create routes for user auth pages and home

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
+Route::group(['namespace' => 'App\Http\Controllers'], function() {
+    Route::view('/', 'index')->name('home.index');
+
+    Route::group(['middleware' => ['guest']], function() {
+        // Register routes
+        Route::get('/register', 'AuthController@show_register')->name('register.show');
+        Route::post('/register', 'AuthController@register')->name('register.perform');
+
+        // Login routes
+        Route::get('/login', 'AuthController@show_login')->name('login.show');
+        Route::post('/login', 'AuthController@login')->name('login.perform');
+    });
+
+    Route::group(['middleware' => ['auth']], function() {
+
+        // Logout routes
+        Route::get('/logout', 'AuthController@logout')->name('logout.perform');
+    });
 });
