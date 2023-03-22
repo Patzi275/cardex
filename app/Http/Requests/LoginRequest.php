3a12cf4f31f13fdb7bc4e394bed81f6d69ec7a5a
patrick <patrickzocli2021@gmail.com>
2023-03-22 23:20:02 +0100

    Ajout des vues et controllers de About et de Contacts

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -11,7 +11,7 @@ class LoginRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,7 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => 'required|email',
+            'email' => 'required|email|exists:users,email',
             'password' => 'required'
         ];
     }
